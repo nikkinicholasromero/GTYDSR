dd550b6a30d4254019cefaded0c707d118df96bc
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2015-08-11 02:22:40 +0800

    SQL Schema
    Committed initial SQL Schema

---
+++ GTYDSR/gty_dsr.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 10, 2015 at 08:14 PM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gty_dsr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE IF NOT EXISTS `bank` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE IF NOT EXISTS `branch` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `location` varchar(50) DEFAULT NULL,
+  `remittance_bank` varchar(50) DEFAULT NULL,
+  `proprietor` varchar(50) DEFAULT NULL,
+  `contact_number` varchar(50) DEFAULT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discrepancy`
+--
+
+CREATE TABLE IF NOT EXISTS `discrepancy` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE IF NOT EXISTS `record` (
+  `id` int(11) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `bank` varchar(50) DEFAULT NULL,
+  `date` date NOT NULL,
+  `consignment` int(11) NOT NULL DEFAULT '0',
+  `overdue` int(11) NOT NULL DEFAULT '0',
+  `advanced` int(11) NOT NULL DEFAULT '0',
+  `open_consignment` int(11) NOT NULL DEFAULT '0',
+  `due_consignment` int(11) NOT NULL DEFAULT '0',
+  `new_consignment` int(11) NOT NULL DEFAULT '0',
+  `sales` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `expense` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `deposit` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `ocoh` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `pcoh` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `acoh` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `diff` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `discrepancy_type` varchar(50) NOT NULL DEFAULT 'None',
+  `discrepancy_category` varchar(50) NOT NULL DEFAULT 'None',
+  `discrepancy_amount` decimal(17,2) NOT NULL DEFAULT '0.00',
+  `additional_information` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `discrepancy`
+--
+ALTER TABLE `discrepancy`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type` (`type`);
+
+--
+-- Indexes for table `record`
+--
+ALTER TABLE `record`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `branch` (`branch`,`date`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `discrepancy`
+--
+ALTER TABLE `discrepancy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `record`
+--
+ALTER TABLE `record`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
